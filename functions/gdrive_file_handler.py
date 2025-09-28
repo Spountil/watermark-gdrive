c7c4c62c98463bc5aff0b009f41547c5b655c554
Spountil <ramana.alwardi@gmail.com>
2025-09-27 19:35:18 -1000

    Correct the last commit

--- functions/gdrive_file_handler.py
+++ functions/gdrive_file_handler.py
@@ -306,9 +306,11 @@ def gdrive_file_handler(resource_id, resource_state, FILE_SAVE_PATH, message_num
 
                 nb_file_uploaded += 1
 
-            end = time.time()
-            timing = end - start
-            db.collection("log_time").document(resource_id).set({'Processing time': timing, 'Number of files downloaded': nb_file_downloaded, 'Number of files watermarked': nb_file_to_mrkd, 'Number of files uploaded': nb_file_uploaded}, merge=True)
+            if nb_file_downloaded or nb_file_to_mrkd or nb_file_uploaded:
+                logging.info(f"Processing summary for resource {resource_id}: Downloaded: {nb_file_downloaded}, Watermarked: {nb_file_to_mrkd}, Uploaded: {nb_file_uploaded}")
+                end = time.time()
+                timing = end - start
+                db.collection("log_time").document(resource_id).set({'Processing time': timing, 'Number of files downloaded': nb_file_downloaded, 'Number of files watermarked': nb_file_to_mrkd, 'Number of files uploaded': nb_file_uploaded}, merge=True)
                 
         else:
             logging.info("No significant changes detected by changes.list() despite notification from the webhook.")
